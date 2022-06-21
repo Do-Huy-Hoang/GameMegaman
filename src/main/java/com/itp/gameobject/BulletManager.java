f78e104d8d1197aaad0e69e7e6c7e0d6c9b0e91f
maixuanvien123 <106679180+maixuanvien123@users.noreply.github.com>
2022-06-21 10:41:57 +0700

    Revert "Update_RobotR_to_Yellow"
    This reverts commit c4acf15b0694cce3879e9259ee64997792f287dc.

--- src/main/java/com/itp/gameobject/BulletManager.java
+++ src/main/java/com/itp/gameobject/BulletManager.java
@@ -5,5 +5,24 @@ import com.itp.state.GameWorldState;
 
 public class BulletManager extends ParticularObjectManager {
 
- 
+    public BulletManager(GameWorldState gameWorld) {
+        super(gameWorld);
+    }
+
+    @Override
+    public void UpdateObjects() {
+        super.UpdateObjects(); 
+        synchronized(particularObjects){
+            for(int id = 0; id < particularObjects.size(); id++){
+                
+                ParticularObject object = particularObjects.get(id);
+                
+                if(object.isObjectOutOfCameraView() || object.getState() == ParticularObject.DEATH){
+                    particularObjects.remove(id);
+                   
+                }
+            }
+        }
+    }
+        
 }
