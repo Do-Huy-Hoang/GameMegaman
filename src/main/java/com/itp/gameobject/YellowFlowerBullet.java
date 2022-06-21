0c45f897a5930ac0b4b1ac2b6acaea23fd46a104
maixuanvien123 <106679180+maixuanvien123@users.noreply.github.com>
2022-06-21 10:42:14 +0700

    Revert "update_RobotR to YellowFlower"
    This reverts commit eee22b84044a6523b198a615ae3d519c104ff473.

--- src/main/java/com/itp/gameobject/YellowFlowerBullet.java
+++ src/main/java/com/itp/gameobject/YellowFlowerBullet.java
@@ -1,50 +1,13 @@
-package com.gamestudio.gameobject;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itp.gameobject;
 
-import com.gamestudio.state.GameWorldState;
-import com.gamestudio.effect.Animation;
-import com.gamestudio.effect.CacheDataLoader;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-
-public class YellowFlowerBullet extends Bullet{
-	
-    private Animation forwardBulletAnim, backBulletAnim;
+/**
+ *
+ * @author Asus
+ */
+public class YellowFlowerBullet {
     
-    public YellowFlowerBullet(float x, float y, GameWorldState gameWorld) {
-            super(x, y, 30, 30, 1.0f, 10, gameWorld);
-            forwardBulletAnim = CacheDataLoader.getInstance().getAnimation("yellow_flower_bullet");
-            backBulletAnim = CacheDataLoader.getInstance().getAnimation("yellow_flower_bullet");
-            backBulletAnim.flipAllImage();
-    }
-
-    
-    
-    @Override
-    public Rectangle getBoundForCollisionWithEnemy() {
-            // TODO Auto-generated method stub
-            return getBoundForCollisionWithMap();
-    }
-
-    @Override
-    public void draw(Graphics2D g2) {
-            // TODO Auto-generated method stub
-        if(getSpeedX() > 0){          
-            forwardBulletAnim.Update(System.nanoTime());
-            forwardBulletAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
-        }else{
-            backBulletAnim.Update(System.nanoTime());
-            backBulletAnim.draw((int) (getPosX() - getGameWorld().camera.getPosX()), (int) getPosY() - (int) getGameWorld().camera.getPosY(), g2);
-        }
-        //drawBoundForCollisionWithEnemy(g2);
-    }
-
-    @Override
-    public void Update() {
-            // TODO Auto-generated method stub
-        super.Update();
-    }
-
-    @Override
-    public void attack() {}
-
 }
